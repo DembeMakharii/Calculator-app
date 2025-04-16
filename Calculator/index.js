42835f8ba051e631afeec02d22adab3a0c6af43d
Dembe Makhari <dembemakhari67@gmail.com>
2025-04-16 14:53:41 +0200

    adding the title

--- Calculator/index.js
+++ Calculator/index.js
@@ -3,11 +3,16 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input) {
-display.value += input; 
+    // Append the input to the display
+     display.value += input;
 }
+ 
 function clearDisplay() {
+    display.value = "";
 
 }
 function calculate() {
+    display.value = eval(display.value);
+    // Evaluate the expression in the display
 
 }
