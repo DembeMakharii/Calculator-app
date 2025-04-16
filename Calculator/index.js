b98d599b25d39079ca9347bd2cd4fd227dbaddec
Dembe Makhari <dembemakhari67@gmail.com>
2025-04-16 16:13:32 +0200

    adding the error option

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,18 +1,20 @@
 // CALCULATOR PROGRAM
 
-const display = document.getElementById("display");
+const display = document.getElementById("Display");
 
 function appendToDisplay(input) {
-    // Append the input to the display
-     display.value += input;
+    display.value += input;
 }
- 
+
 function clearDisplay() {
     display.value = "";
-
 }
-function calculate() {
-    display.value = eval(display.value);
-    // Evaluate the expression in the display
 
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
 }
